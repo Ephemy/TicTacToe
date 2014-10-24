7278155d4958bd8c0020aa213ed271a7e2e1cf43
Jonathan Chou <jonathanchou@Jonathans-MacBook-Pro-2.local>
2014-10-23 17:36:22 -0700

    First Commit of the weekend baby

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,6 +9,17 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labelOne;
+@property (weak, nonatomic) IBOutlet UILabel *labelTwo;
+@property (weak, nonatomic) IBOutlet UILabel *labelThree;
+@property (weak, nonatomic) IBOutlet UILabel *labelFour;
+@property (weak, nonatomic) IBOutlet UILabel *labelFive;
+@property (weak, nonatomic) IBOutlet UILabel *labelSix;
+@property (weak, nonatomic) IBOutlet UILabel *labelSeven;
+@property (weak, nonatomic) IBOutlet UILabel *labelEight;
+@property (weak, nonatomic) IBOutlet UILabel *labelNine;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property (nonatomic) BOOL isPlayerX;
 
 @end
 
@@ -22,6 +33,48 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)findLabelUsingPoint: (CGPoint)point{
+    if(CGRectContainsPoint(self.labelOne.frame, point)){
+        [self labelPressed:self.labelOne];}
+    if(CGRectContainsPoint(self.labelTwo.frame, point)){
+        [self labelPressed:self.labelTwo];}
+    if(CGRectContainsPoint(self.labelThree.frame, point)){
+        [self labelPressed:self.labelThree];}
+    if(CGRectContainsPoint(self.labelFour.frame, point)){
+        [self labelPressed:self.labelFour];}
+    if(CGRectContainsPoint(self.labelFive.frame, point)){
+        [self labelPressed:self.labelFive];}
+    if(CGRectContainsPoint(self.labelSix.frame, point)){
+        [self labelPressed:self.labelSix];}
+    if(CGRectContainsPoint(self.labelSeven.frame, point)){
+        [self labelPressed:self.labelSeven];}
+    if(CGRectContainsPoint(self.labelEight.frame, point)){
+        [self labelPressed:self.labelEight];}
+    if(CGRectContainsPoint(self.labelNine.frame, point)){
+        [self labelPressed:self.labelNine];}
+}
+
+- (void)labelPressed:(UILabel *)labelPressed{
+//if label 1-9 countains cgpoint(mouse) - change to x or o
+//if already x or o do nothing.
+//change bool value
+    
+    //BOOL isPlayerX = YES;
+    //if([self buttonPressed]){
+    if(self.isPlayerX){
+        labelPressed.text = @"X";}
+    else{labelPressed.text = @"O";}
+    self.isPlayerX = !(self.isPlayerX);
+    }
+
+    
+
+
+- (IBAction)onLabelTapped:(id)sender {
+    CGPoint point = [sender locationInView:self.view];
+    [self findLabelUsingPoint:point];
 }
 
 @end
